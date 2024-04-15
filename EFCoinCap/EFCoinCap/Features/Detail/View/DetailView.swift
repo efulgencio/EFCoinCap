b0e2fddf1a7b89ae401e3ef3e55573774001b9be
eduardofulgenciocomendeiro <eduardofulgenciocomendeiro@gmail.com>
2024-04-15 20:17:53 +0200

    Update DetailView.swift
    Added view for show detail

--- EFCoinCap/EFCoinCap/Features/Detail/View/DetailView.swift
+++ EFCoinCap/EFCoinCap/Features/Detail/View/DetailView.swift
@@ -111,3 +111,37 @@ extension Text {
         self.modifier(CustomTextStyle())
     }
 }
+
+
+struct TextValueRow: View {
+    
+    let textItem: String
+    let valueItem: String
+    
+    var body: some View {
+        HStack(spacing: 0) {
+            textItemColumn
+            Spacer()
+            valueItemColumn
+        }
+        .padding()
+        .frame(height: 40)
+    }
+}
+
+extension TextValueRow {
+    
+    private var textItemColumn: some View {
+        HStack(spacing: 0) {
+            Text(textItem)
+                .bold()
+        }
+    }
+    
+    private var valueItemColumn: some View {
+        VStack(alignment: .trailing) {
+            Text(valueItem)
+        }
+    }
+}
+
