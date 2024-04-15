cb48d67de9c54e762513de5331877f448c3acc2f
eduardofulgenciocomendeiro <eduardofulgenciocomendeiro@gmail.com>
2024-04-15 20:07:36 +0200

    Add files via upload
    Text  and value row for detail view.

---
+++ EFCoinCap/EFCoinCap/CustomViews/TextValueRow.swift
@@ -0,0 +1,46 @@
+//
+//  DetailRow.swift
+//  EFCoinCap
+//
+//  Created by efulgencio on 15/4/24.
+//
+
+import SwiftUI
+
+struct TextValueRow: View {
+    
+    let textItem: String
+    let valueItem: String
+    
+    var body: some View {
+        HStack(spacing: 0) {
+            textItemColumn
+            Spacer()
+            valueItemColumn
+        }
+        .padding()
+        .frame(height: 40)
+    }
+}
+
+extension TextValueRow {
+    
+    private var textItemColumn: some View {
+        HStack(spacing: 0) {
+            Text(textItem)
+                .bold()
+        }
+    }
+    
+    private var valueItemColumn: some View {
+        VStack(alignment: .trailing) {
+            Text(valueItem)
+        }
+    }
+}
+
+
+
+#Preview {
+    TextValueRow(textItem: "Item text", valueItem: "Item value")
+}
